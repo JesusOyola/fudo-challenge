c1046c3f5c91471c85ac36f1bd5b7bd969b2cf7b
JesusOyola <jesus.oyola@interatica.com>
2025-07-21 17:48:38 -0300

    configure general sidebar options

---
+++ src/app/pages/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {
+  private router = inject(Router)
+
+  getTweets() {
+    console.log('getTweets llamado');
+    // Tu lógica aquí
+  }
+
+  isActive(path: string): boolean {
+    return this.router.url === path;
+  }
+}
